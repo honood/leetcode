7e78456e6e4354eb6a445c538926d28420f9115d
HoNooD <honood@gmail.com>
2024-05-20 22:54:41 +0800

    todo: 310. Minimum Height Trees

---
+++ C++/0310-minimum-height-trees.cpp
@@ -0,0 +1,12 @@
+// HoNooD
+// 2024.05.20 22:53
+
+// 310. Minimum Height Trees
+// https://leetcode.com/problems/minimum-height-trees/description/
+
+class Solution {
+public:
+  vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+    // TODO:
+  }
+};
