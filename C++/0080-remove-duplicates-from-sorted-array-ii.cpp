d1cbd916472b4ed7f4c1694303b8040f03416610
HoNooD <honood@gmail.com>
2024-06-05 11:51:20 +0800

    80. Remove Duplicates from Sorted Array II & update: 26. Remove Duplicates from Sorted Array

--- C++/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ C++/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,8 +1,8 @@
 // HoNooD
-// 2024.06.05 09:31
+// 2024.06.05 10:38
 
-// 26. Remove Duplicates from Sorted Array
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+// 80. Remove Duplicates from Sorted Array II
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
 
 auto __unsync_ios_stdio = ios::sync_with_stdio(false);
 auto __untie_cin = cin.tie(nullptr);
@@ -11,13 +11,13 @@ class Solution {
 public:
   int removeDuplicates(vector<int>& nums) {
     int n = nums.size();
-    if (n <= 1) {
+    if (n <= 2) {
       return n;
     }
 
-    int overwrite = 1;
-    for (int i = 1; i < n; ++i) {
-      if (nums[i] != nums[overwrite - 1]) {
+    int overwrite = 2;
+    for (int i = 2; i < n; ++i) {
+      if (nums[i] != nums[overwrite - 2]) {
         if (overwrite != i) {
           nums[overwrite] = nums[i];
         }
