14befc4e6d3754b392aed0cc1a94f91e2997d6eb
HoNooD <honood@gmail.com>
2024-06-10 12:46:29 +0800

    update: 1. Two Sum

--- C++/0001-two-sum.cpp
+++ C++/0001-two-sum.cpp
@@ -1,23 +1,22 @@
-// HoNooD
+// HoNooD <honood@gmail.com>
 // 2022.06.04 17:10
 
-// https://leetcode.com/problems/two-sum/
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/?envType=study-plan-v2&envId=top-interview-150
 
 class Solution {
 public:
   vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> m{};
-
+    unordered_map<int, int> numToIndex{};
     for (int i = 0; i < nums.size(); ++i) {
-      int left = nums[i];
-      int right = target - left;
-      if (m.find(right) != m.end()) {
-        return {m[right], i};
-      } 
+      int complement = target - nums[i];
+      if (numToIndex.contains(complement)) {
+        return {numToIndex[complement], i};
+      }
 
-      m[left] = i;
+      numToIndex[nums[i]] = i;
     }
 
-    return {-1, -1};
+    return {};
   }
 };
