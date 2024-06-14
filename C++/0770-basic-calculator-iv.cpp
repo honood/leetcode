6fdd6f6595612b0d7622ad9b6325591ac85799ff
HoNooD <honood@gmail.com>
2024-06-14 11:29:28 +0800

    todo: 770. Basic Calculator IV

---
+++ C++/0770-basic-calculator-iv.cpp
@@ -0,0 +1,15 @@
+// HoNooD <honood@gmail.com>
+// 2024.06.14 11:30
+
+// 770. Basic Calculator IV
+// https://leetcode.com/problems/basic-calculator-iv/description/
+
+auto __unsync_ios_stdio = ios::sync_with_stdio(false);
+auto __untie_cin = cin.tie(nullptr);
+
+class Solution {
+public:
+  vector<string> basicCalculatorIV(string expression, vector<string>& evalvars, vector<int>& evalints) {
+
+  }
+};
