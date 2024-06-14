39f1aea674f6fd65c308d32eb08c0b912d9f17fc
HoNooD <honood@gmail.com>
2024-06-14 11:33:09 +0800

    todo: 282. Expression Add Operators

---
+++ C++/0282-expression-add-operators.cpp
@@ -0,0 +1,14 @@
+// HoNooD <honood@gmail.com>
+// 2024.06.14 11:30
+
+// 282. Expression Add Operators
+// https://leetcode.com/problems/expression-add-operators/description/
+
+auto __unsync_ios_stdio = ios::sync_with_stdio(false);
+auto __untie_cin = cin.tie(nullptr);
+
+class Solution {
+public:
+  vector<string> addOperators(string num, int target) {
+  }
+};
