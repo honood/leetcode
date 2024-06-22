4134242c2f61d1c425c2ba723279dfe56c4110b6
HoNooD <honood@gmail.com>
2024-06-22 11:40:30 +0800

    todo: 2503. Maximum Number of Points From Grid Queries

---
+++ C++/2503-maximum-number-of-points-from-grid-queries.cpp
@@ -0,0 +1,15 @@
+// HoNooD <honood@gmail.com>
+// 2024.06.21 18:30
+
+// 2503. Maximum Number of Points From Grid Queries
+// https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/description/
+
+auto __unsync_ios_stdio = ios::sync_with_stdio(false);
+auto __untie_cin = cin.tie(nullptr);
+
+class Solution {
+public:
+  vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+
+  }
+};
