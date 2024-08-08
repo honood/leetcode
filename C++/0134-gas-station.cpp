43000d724e4dcff0ef6617304da8b9ea91f3a898
HoNooD <honood@gmail.com>
2024-08-08 16:00:30 +0800

    update: 134. Gas Station

--- C++/0134-gas-station.cpp
+++ C++/0134-gas-station.cpp
@@ -4,16 +4,23 @@
 // 134. Gas Station
 // https://leetcode.com/problems/gas-station/description/?envType=study-plan-v2&envId=top-interview-150
 
-// https://en.wikipedia.org/wiki/Greedy_algorithm
-// https://en.wikipedia.org/wiki/Glossary_of_mathematical_symbols
-
 auto __unsync_ios_stdio = ios::sync_with_stdio(false);
 auto __untie_cin = cin.tie(nullptr);
 
-// Do not use, will TLE (Time Limit Exceeded)
 class Solution {
 public:
   int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    return can_complete_circuit_v2(gas, cost);
+  }
+
+private:
+  // Brute Force
+  // Note: result in Time Limit Exceeded (TLE)
+  //
+  // Complexity Analysis:
+  // - Time complexity: O(n^2)
+  // - Space complexity: O(1)
+  int can_complete_circuit_v1(vector<int> const& gas, vector<int> const& cost) {
     int n = gas.size();
     assert(n == cost.size());
 
@@ -34,50 +41,57 @@ public:
 
     return -1;
   }
-};
 
-class Solution_2 {
-public:
-  int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+  // Greedy algorithm
+  // https://leetcode.com/tag/greedy/
+  //
+  // Complexity Analysis:
+  // - Time complexity: O(n)
+  // - Space complexity: O(1)
+  int can_complete_circuit_v2(vector<int> const& gas, vector<int> const& cost) {
     int n = gas.size();
     assert(n == cost.size());
 
-    int total_tank = 0; // total surplus/deficit
-    int curr_tank = 0; // current surplus/deficit
-    int start = 0; // start station
+    int total_tank = 0; // total net gasoline
+    int curr_tank = 0; // current net gasoline
+    int start = 0;
 
     for (int i = 0; i < n; ++i) {
-      int remain = gas[i] - cost[i];
-      total_tank += remain;
-      curr_tank += remain;
+      int net_gas = gas[i] - cost[i];
+      total_tank += net_gas;
+      curr_tank += net_gas;
 
-      // If `current_tank` becomes negative after station `i`, it indicates that
-      // the fuel accumulated from the current starting point `start` to `i` is
+      // If `curr_tank` becomes negative after station `i`, it means that the
+      // fuel accumulated from the current starting station `start` to `i` is
       // insufficient to reach station `i + 1`.
       //
-      // This implies that no station in the segment from the current `start` to
-      // `i` can be a valid starting point. If you can't reach station `i` from
-      // the current `start`, then starting from any station between `start` and
-      // `i` will also fail to reach beyond `i`, because those stations have
-      // even less fuel (as `current_tank` is already negative at `i`).
+      // This implies that no station in the range from `start` to `i` can serve
+      // as a valid starting point for completing the circuit.
       //
-      // Consequently, you should move the starting point to `i + 1`, the next
-      // station after the failure point, and reset `current_tank` to `0`. This
-      // is because `i + 1` is the next candidate for a potential starting point
-      // where you hope to accumulate enough fuel to complete the circuit.
+      // Here's a simple proof:
+      // Suppose `a`, `b`, `c`, `d`, `e` represent the net fuel from station
+      // `station` to station `i`:
+      //   a >= 0
+      //   a + b >= 0
+      //   a + b + c >= 0
+      //   a + b + c + d >= 0
+      //   a + b + c + d + e < 0
+      // Therefore:
+      //   b + c + d + e = (a + b + c + d + e) - a < 0
+      //   c + d + e = (a + b + c + d + e) - (a + b) < 0
+      //   d + e = (a + b + c + d + e) - (a + b + c) < 0
+      //   e = (a + b + c + d + e) - (a + b + c + d) < 0
+      // Thus, no station from `start` to `i` can reach `i + 1`.
+      //
+      // Consequently, shift the starting point to `i + 1` and reset `curr_tank`
+      // to zero. This indicates that `i + 1` is the next potential candidate
+      // for a valid starting point.
       if (curr_tank < 0) {
         start = i + 1;
         curr_tank = 0;
       }
     }
 
-    // `total_tank >= 0` provides a global check ensuring there’s enough overall
-    // fuel to cover the entire route, even if local segments failed during
-    // traversal.
-    //
-    // By combining the local segment checks (`current_tank`) with the global
-    // sufficiency check (`total_tank`), the algorithm effectively determines a
-    // feasible start point for the circular journey.
     return total_tank >= 0 ? start : -1;
   }
 };
