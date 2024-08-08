035628e6fd137139411436fa0b9c8603346e93af
HoNooD <honood@gmail.com>
2024-08-08 15:10:48 +0800

    update: 238. Product of Array Except Self

--- C++/0238-product-of-array-except-self.cpp
+++ C++/0238-product-of-array-except-self.cpp
@@ -7,71 +7,83 @@
 // Note: You must write an algorithm that runs in O(n) time and without using
 // the division operation.
 
-// https://en.wikipedia.org/wiki/Prefix_sum
-
 auto __unsync_ios_stdio = ios::sync_with_stdio(false);
 auto __untie_cin = cin.tie(nullptr);
 
-// We can use two auxiliary arrays to store the product of all elements to the
-// left and right of each element, and then combine these two arrays to get the
-// result.
-//
-// 1. Construct Left and right product arrays:
-//   - `left_products[i]` stores the product of all elements from the start up
-//     to `i - 1`.
-//   - `right_products[i]` stores the product of all elements from `i + 1` to
-//     the end.
-// 2. Combine arrays: `answer[i] = left_products[i] * right_products[i]`
 class Solution {
 public:
   vector<int> productExceptSelf(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> leftProd(n, 1);
-    vector<int> rightProd(n, 1);
-    vector<int> res(n);
-
-    for (int i = 1; i < n; ++i) {
-      leftProd[i] = leftProd[i - 1] * nums[i - 1];
-    }
-
-    for (int i = n - 2; i >= 0; --i) {
-      rightProd[i] = rightProd[i + 1] * nums[i + 1];
-    }
-
-    for (int i = 0; i < n; ++i) {
-      res[i] = leftProd[i] * rightProd[i];
-    }
-
-    return res;
+    return product_except_self_v2(nums);
   }
-};
 
-// Great!
-//
-// solved with O(1) extra space complexity (The output array does not count as
-// extra space for space complexity analysis.)
-class Solution_2 {
-public:
-  vector<int> productExceptSelf(vector<int>& nums) {
+private:
+  // Prefix Sum
+  // https://leetcode.com/tag/prefix-sum/
+  //
+  // We can use two auxiliary arrays to store the product of all elements to the
+  // left and right of each element, and then combine these two arrays to get
+  // the result.
+  //
+  // 1. Construct left and right product arrays:
+  //   - `left_products[i]` stores the product of all elements from the start up
+  //     to `i - 1`.
+  //   - `right_products[i]` stores the product of all elements from `i + 1` to
+  //     the end.
+  // 2. Combine arrays: `answer[i] = left_products[i] * right_products[i]`
+  //
+  // Complexity Analysis:
+  // - Time complexity: O(n)
+  // - Space complexity: O(n)
+  vector<int> product_except_self_v1(vector<int> const& nums) {
     int n = nums.size();
-    vector<int> res(n, 1);
 
-    // Calculate left products: `res[i]` is the product of elements to the left
-    // of `i`
-    //
-    // Note: `res[0]` remains `1` since there are no elements to the left
+    vector<int> left_products(n, 1);
     for (int i = 1; i < n; ++i) {
-      res[i] = res[i - 1] * nums[i - 1];
+      left_products[i] = left_products[i - 1] * nums[i - 1];
     }
 
-    // Calculate right products: `rightProd` accumulates the product of elements
-    // to the right of `i`
-    int rightProd = 1;
+    vector<int> right_products(n, 1);
+    for (int i = n - 2; i >= 0; --i) {
+      right_products[i] = right_products[i + 1] * nums[i + 1];
+    }
+
+    vector<int> answer(n);
+    for (int i = 0; i < n; ++i) {
+      answer[i] = left_products[i] * right_products[i];
+    }
+
+    return answer;
+  }
+
+  // Prefix Sum
+  // https://leetcode.com/tag/prefix-sum/
+  //
+  // Follow up: Can you solve the problem in `O(1)` extra space complexity? (The
+  // output array does not count as extra space for space complexity analysis.)
+  //
+  // Complexity Analysis:
+  // - Time complexity: O(n)
+  // - Space complexity: O(1)
+  vector<int> product_except_self_v2(vector<int> const& nums) {
+    int n = nums.size();
+    vector<int> answer(n, 1);
+
+    // Calculate left products: `answer[i]` is the product of elements to the
+    // left of `i`
+    //
+    // Note: `answer[0]` remains `1` since there are no elements to the left
+    for (int i = 1; i < n; ++i) {
+      answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    // Calculate right products: `right_products` accumulates the product of
+    // elements to the right of `i`
+    int right_products = 1;
     for (int i = n - 1; i >= 0; --i) {
-      res[i] *= rightProd;
-      rightProd *= nums[i];
+      answer[i] *= right_products;
+      right_products *= nums[i];
     }
 
-    return res;
+    return answer;
   }
 };
