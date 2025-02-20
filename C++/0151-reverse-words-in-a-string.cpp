2a8a7d9dcc7aacec020a44ca3fd9742ef11e37d8
HoNooD <honood@gmail.com>
2025-02-21 00:06:21 +0800

    update: 151. Reverse Words in a String

--- C++/0151-reverse-words-in-a-string.cpp
+++ C++/0151-reverse-words-in-a-string.cpp
@@ -3,13 +3,50 @@
 
 // 151. Reverse Words in a String
 // https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
+// https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
 
-auto __unsync_ios_stdio = ios::sync_with_stdio(false);
-auto __untie_cin = cin.tie(nullptr);
+auto __unsync_with_stdio = std::ios::sync_with_stdio(false);
+auto __uncin_tie = std::cin.tie(nullptr);
 
 class Solution {
 public:
   string reverseWords(string s) {
+    return reverse_words_v2(std::move(s));
+  }
+
+private:
+  string reverse_words_v1(string s) {
+    if (s.empty()) {
+      return s;
+    }
+
+    string result{};
+    string word{};
+    for (char c : s) {
+      if (c != ' ') {
+        word += c;
+      } else if (!word.empty()) {
+        if (!result.empty()) {
+          word += ' ';
+        }
+        word += result;
+        result.swap(word);
+        word.clear();
+      }
+    }
+
+    if (!word.empty()) {
+      if (!result.empty()) {
+        word += ' ';
+      }
+      word += result;
+      result.swap(word);
+    }
+
+    return result;
+  }
+
+  string reverse_words_v2(string s) {
     if (s.empty()) {
       return s;
     }
@@ -17,11 +54,12 @@ public:
     vector<string> words{};
     int all_words_len = 0;
     // `std::stringstream` is also suitable
-    std::istringstream ss{s};
+    std::istringstream iss{s};
     string word{};
     // https://en.cppreference.com/w/cpp/string/basic_string/operator_ltltgtgt
     // https://en.cppreference.com/w/cpp/io/basic_ios/operator_bool
-    while (ss >> word) {
+    // https://en.cppreference.com/w/cpp/io/basic_ios/eof#See_also
+    while (iss >> word) {
       all_words_len += word.size();
       words.emplace_back(std::move(word));
     }
@@ -30,38 +68,34 @@ public:
       return {};
     }
 
-    string res{};
-    res.reserve(all_words_len + words.size() - 1);
+    string result{};
+    result.reserve(all_words_len + words.size() - 1);
     for (int i = words.size() - 1; i >= 0; --i) {
-      if (!res.empty()) {
-        res += ' ';
+      if (!result.empty()) {
+        result += ' ';
       }
-      res += words[i];
+      result += words[i];
     }
 
-    return res;
+    return result;
   }
-};
 
-// Efficient and less memory usage, but more complex and less intuitive.
-class Solution_2 {
-public:
-  string reverseWords(string s) {
+  string reverse_words_v3(string s) {
     if (s.empty()) {
       return s;
     }
 
-    string res{};
+    string result{};
     string word{};
     for (int i = s.size() - 1; i >= -1; --i) {
       if (i >= 0 && s[i] != ' ') {
         word += s[i];
       } else if (!word.empty()) {
-        if (!res.empty()) {
-          res += ' ';
+        if (!result.empty()) {
+          result += ' ';
         }
         for (int j = word.size() - 1; j >= 0; --j) {
-          res += word[j];
+          result += word[j];
         }
 
         if (i != -1) {
@@ -70,51 +104,40 @@ public:
       }
     }
 
-    return res;
+    return result;
   }
-};
 
-// Not recommended: more memory is used due to repeated string allocations
-// (`res = word + ' ' + res;`) in the loop. Each concatenation operation
-// allocates additional memory, leading to increased memory usage, especially
-// with larger inputs.
-class Solution_3 {
-public:
-  string reverseWords(string s) {
+  string reverse_words_v4(string s) {
     if (s.empty()) {
       return s;
     }
 
-    string res{};
+    string result{};
     // `std::stringstream` is also suitable
     std::istringstream ss{s};
     string word{};
     // https://en.cppreference.com/w/cpp/string/basic_string/operator_ltltgtgt
     // https://en.cppreference.com/w/cpp/io/basic_ios/operator_bool
+    // https://en.cppreference.com/w/cpp/io/basic_ios/eof#See_also
     while (ss >> word) {
-      if (res.empty()) {
-        res = std::move(word);
+      if (result.empty()) {
+        result = std::move(word);
       } else {
-        res = word + ' ' + res;
+        word += ' ';
+        word += result;
+        result.swap(word);
       }
     }
 
-    return res;
+    return result;
   }
-};
 
-// Not recommended: more memory is used due to repeated string allocations
-// (`res = word + ' ' + res;`) in the loop. Each concatenation operation
-// allocates additional memory, leading to increased memory usage, especially
-// with larger inputs.
-class Solution_4 {
-public:
-  string reverseWords(string s) {
+  string reverse_words_v5(string s) {
     if (s.empty()) {
       return s;
     }
 
-    string res{};
+    string result{};
     string word{};
     int n = s.size();
     for (int i = 0; i < n; ++i) {
@@ -124,41 +147,12 @@ public:
       }
 
       if (!word.empty()) {
-        if (res.empty()) {
-          res = std::move(word);
+        if (result.empty()) {
+          result = std::move(word);
         } else {
-          res = word + ' ' + res;
-        }
-        word.clear();
-      }
-    }
-
-    return res;
-  }
-};
-
-// Not recommended: more memory is used due to repeated string allocations
-// (`res = word + ' ' + res;`) in the loop. Each concatenation operation
-// allocates additional memory, leading to increased memory usage, especially
-// with larger inputs.
-class Solution_5 {
-public:
-  string reverseWords(string s) {
-    if (s.empty()) {
-      return s;
-    }
-
-    string res{};
-    string word{};
-    int n = s.size();
-    for (int i = 0; i <= n; ++i) {
-      if (i < n && s[i] != ' ') {
-        word += s[i];
-      } else if (!word.empty()) {
-        if (res.empty()) {
-          res = std::move(word);
-        } else {
-          res = word + ' ' + res;
+          word += ' ';
+          word += result;
+          result.swap(word);
         }
 
         if (i != n) {
@@ -167,6 +161,35 @@ public:
       }
     }
 
-    return res;
+    return result;
+  }
+
+  string reverse_words_v6(string s) {
+    if (s.empty()) {
+      return s;
+    }
+
+    string result{};
+    string word{};
+    int n = s.size();
+    for (int i = 0; i <= n; ++i) {
+      if (i < n && s[i] != ' ') {
+        word += s[i];
+      } else if (!word.empty()) {
+        if (result.empty()) {
+          result = std::move(word);
+        } else {
+          word += ' ';
+          word += result;
+          result.swap(word);
+        }
+
+        if (i != n) {
+          word.clear();
+        }
+      }
+    }
+
+    return result;
   }
 };
