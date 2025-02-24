4550fba0545ce74340ba93c43a2b61423a82fbd3
HoNooD <honood@gmail.com>
2025-02-24 19:06:09 +0800

    update: 11. Container With Most Water

--- C++/0011-container-with-most-water.cpp
+++ C++/0011-container-with-most-water.cpp
@@ -1,77 +1,32 @@
 // HoNooD <honood@gmail.com>
-// 2024.06.20 08:30
+// 2025.02.24 18:30
 
 // 11. Container With Most Water
 // https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
-//
-// You are given an integer array height of length `n`. There are `n` vertical
-// lines drawn such that the two endpoints of the ith line are `(i, 0)` and
-// `(i, height[i])`.
-//
-// Find two lines that together with the x-axis form a container, such that the
-// container contains the most water.
-//
-// Return the maximum amount of water a container can store.
-//
-// Notice that you may not slant the container.
+// https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
 
-auto __unsync_ios_stdio = ios::sync_with_stdio(false);
-auto __untie_cin = cin.tie(nullptr);
+auto __unsync_with_stdio = std::ios::sync_with_stdio(false);
+auto __uncin_tie = std::cin.tie(nullptr);
 
 class Solution {
 public:
   int maxArea(vector<int>& height) {
-    int n = height.size();
-    if (n <= 1) {
-      return 0;
-    }
+    return max_area_v1(height);
+  }
 
-    int maxArea = std::numeric_limits<int>::min();
-    int left = 0;
-    int right = n - 1;
-    while (left < right) {
-      int area = std::min(height[left], height[right]) * (right - left);
-      maxArea = std::max(maxArea, area);
+private:
+  int max_area_v1(vector<int> const& height) {
+    int max_area = 0;
+    for (int left = 0, right = height.size() - 1; left < right;) {
+      int current_area = std::min(height[left], height[right]) * (right - left);
+      max_area = std::max(max_area, current_area);
 
-      // To maximize the area, one pointer must be moved:
-      // 1. Regardless of whether `left` or `right` is moved, `(right - left)`
-      //    will always decrease by `1`. Hence, from the perspective of the
-      //    change in `(right - left)`, moving either has the same effect.
-      // 2. Suppose `height[left] < height[right]`.
-      //    - If `right` is moved, `min(height[left], height[right - 1])` might
-      //      be smaller or remain the same as before.
-      //    - If `left` is moved, `min(height[left + 1], height[right])` might
-      //      be larger, smaller, or the same as before.
-      //    Thus, to maximize the area, move `left`.
       if (height[left] < height[right]) {
         ++left;
       } else {
         --right;
       }
     }
-
-    return maxArea;
-  }
-};
-
-// Brute Force
-// Do not use, will TLE (Time Limit Exceeded).
-class Solution_2 {
-public:
-  int maxArea(vector<int>& height) {
-    int n = height.size();
-    if (n <= 1) {
-      return 0;
-    }
-
-    int maxArea = std::numeric_limits<int>::min();
-    for (int i = 0; i < n - 1; ++i) {
-      for (int j = i + 1; j < n; ++j) {
-        int area = std::min(height[i], height[j]) * (j - i);
-        maxArea = std::max(maxArea, area);
-      }
-    }
-
-    return maxArea;
+    return max_area;
   }
 };
