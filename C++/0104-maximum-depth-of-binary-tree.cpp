c2a0b64b5c57265c9e72efff636390eed9cbaad4
HoNooD <honood@gmail.com>
2025-03-03 16:51:22 +0800

    update: 104. Maximum Depth of Binary Tree

--- C++/0104-maximum-depth-of-binary-tree.cpp
+++ C++/0104-maximum-depth-of-binary-tree.cpp
@@ -1,85 +1,94 @@
-// HoNooD
-// 2024.05.08 15:35
+// HoNooD <honood@gmail.com>
+// 2025.03.03 14:20
 
+// 104. Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=leetcode-75
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-interview-150
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *	 int val;
- *	 TreeNode *left;
- *	 TreeNode *right;
- *	 TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *	 TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *	 TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+auto __unsync_with_stdio = std::ios::sync_with_stdio(false);
+auto __uncin_tie = std::cin.tie(nullptr);
+
+// Definition for a binary tree node.
+//
+// struct TreeNode {
+//   int val;
+//   TreeNode* left;
+//   TreeNode* right;
+//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//   TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left),
+//   right(right) {}
+// };
+
 class Solution {
 public:
-	int maxDepth(TreeNode* root) {
-		if (root == nullptr) {
-			return 0;
-		}
+  int maxDepth(TreeNode* root) {
+    return max_depth_v1(root);
+  }
 
-		return 1 + max(maxDepth(root->left), maxDepth(root->right));
-	}
-};
+private:
+  int max_depth_v1(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
 
-class Solution_2 {
-public:
-	int maxDepth(TreeNode* root) {
-		if (root == nullptr) {
-			return 0;
-		}
+    int left_depth = max_depth_v1(root->left);
+    int right_depth = max_depth_v1(root->right);
 
-		queue<TreeNode*> queue{};
-		queue.push(root);
-		int depth = 0;
-		while (!queue.empty()) {
-			int sz = queue.size();
-			for (int i = 0; i < sz; ++i) {
-				TreeNode* node = queue.front();
-				queue.pop();
-				if (node->left != nullptr) {
-					queue.push(node->left);
-				}
-				if (node->right != nullptr) {
-					queue.push(node->right);
-				}
-			}
-			++depth;
-		}
+    return 1 + std::max(left_depth, right_depth);
+  }
 
-		return depth;
-	}
-};
+  int max_depth_v2(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
 
-class Solution_3 {
-public:
-	int maxDepth(TreeNode* root) {
-		if (root == nullptr) {
-			return 0;
-		}
+    int depth = 0;
+    std::queue<TreeNode*> queue{};
+    queue.push(root);
 
-		stack<pair<TreeNode*, int>> stack{};
-		stack.push(make_pair(root, 1));
-		int maxDepth = 1;
-		while (!stack.empty()) {
-			auto top = stack.top();
-			stack.pop();
-			TreeNode* node = top.first;
-			int depth = top.second;
-			if (depth > maxDepth) {
-				maxDepth = depth;
-			}
-			if (node->right != nullptr) {
-				stack.push(make_pair(node->right, depth + 1));
-			}
-			if (node->left != nullptr) {
-				stack.push(make_pair(node->left, depth + 1));
-			}
-		}
+    while (!queue.empty()) {
+      int level_size = queue.size();
+      while (level_size-- > 0) {
+        TreeNode* node = queue.front();
+        queue.pop();
 
-		return maxDepth;
-	}
+        if (node->left != nullptr) {
+          queue.push(node->left);
+        }
+        if (node->right != nullptr) {
+          queue.push(node->right);
+        }
+      }
+
+      ++depth;
+    }
+
+    return depth;
+  }
+
+  int max_depth_v3(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+
+    int max_depth = 0;
+    std::stack<std::pair<TreeNode*, int>> stack{{{root, 1}}};
+
+    while (!stack.empty()) {
+      auto [node, depth] = stack.top();
+      stack.pop();
+
+      max_depth = std::max(max_depth, depth);
+
+      if (node->right != nullptr) {
+        stack.emplace(node->right, depth + 1);
+      }
+      if (node->left != nullptr) {
+        stack.emplace(node->left, depth + 1);
+      }
+    }
+
+    return max_depth;
+  }
 };
