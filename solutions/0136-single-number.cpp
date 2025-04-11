abe26400d3448768c4e83cb801763056cc7b5446
HoNooD <honood@gmail.com>
2025-04-11 14:57:47 +0800

    update: 136. Single Number

--- solutions/0136-single-number.cpp
+++ solutions/0136-single-number.cpp
@@ -1,8 +1,10 @@
-// HoNooD
+// HoNooD <honood@gmail.com>
 // 2024.05.30 15:12
+// 2025.04.11 14:30
 
 // 136. Single Number
 // https://leetcode.com/problems/single-number/description/?envType=study-plan-v2&envId=top-interview-150
+// https://leetcode.com/problems/single-number/?envType=study-plan-v2&envId=leetcode-75
 
 // https://en.wikipedia.org/wiki/Bitwise_operation
 // https://en.wikipedia.org/wiki/Bitwise_operations_in_C
@@ -94,19 +96,89 @@
 // 8. x + y = (x ^ y) + ((x & y) << 1)
 // 9. x - y = ~(~x + y)
 
-auto __unsync_ios_stdio = ios::sync_with_stdio(false);
-auto __untie_cin = cin.tie(nullptr);
+#include <cinttypes>
+#include <functional>
+auto __unsync_with_stdio = std::ios::sync_with_stdio(false);
+auto __uncin_tie = std::cin.tie(nullptr);
 
 class Solution {
 public:
   int singleNumber(vector<int>& nums) {
+    return single_number_v1(nums);
+  }
+
+private:
+  // - Time complexity: O(n)
+  // - Space complexity: O(1)
+  int single_number_v1(vector<int> const& nums) {
     assert(!nums.empty());
 
-    int res = 0;
-    for (auto const& num : nums) {
-      res ^= num;
+    int result = 0;
+    for (int num : nums) {
+      result ^= num;
+    }
+    return result;
+  }
+
+  // - Time complexity: O(n)
+  // - Space complexity: O(1)
+  int single_number_v2(vector<int> const& nums) {
+    assert(!nums.empty());
+    return std::reduce(nums.cbegin(), nums.cend(), 0, std::bit_xor<>{});
+  }
+
+  // - Time complexity: O(n)
+  // - Space complexity: O(n)
+  int single_number_v3(vector<int> const& nums) {
+    assert(!nums.empty());
+
+    unordered_map<int, int> counts{};
+    for (int num : nums) {
+      ++counts[num];
     }
 
-    return res;
+    for (auto const& [num, count] : counts) {
+      if (count == 1) {
+        return num;
+      }
+    }
+
+    return -1;
+  }
+
+  // - Time complexity: O(n)
+  // - Space complexity: O(n)
+  int single_number_v4(vector<int> const& nums) {
+    assert(!nums.empty());
+
+    unordered_set<int> uniq_nums{};
+    long long sum_all = 0;
+    for (int num : nums) {
+      uniq_nums.emplace(num);
+      sum_all += num;
+    }
+
+    long long sum_uniq = 0;
+    for (int num : uniq_nums) {
+      sum_uniq += num;
+    }
+
+    return sum_uniq * 2 - sum_all;
+  }
+
+  // - Time complexity: O(n * log(n))
+  // - Space complexity: O(1)
+  int single_number_v5(vector<int>& nums) {
+    assert(!nums.empty());
+
+    std::sort(nums.begin(), nums.end());
+
+    for (int i = 0, n = nums.size(); i < n; i += 2) {
+      if (i == n - 1 || nums[i] != nums[i + 1]) {
+        return nums[i];
+      }
+    }
+
+    return -1;
   }
 };
